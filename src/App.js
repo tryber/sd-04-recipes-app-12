126ee87813d3085ea88fbc3df4edab38cb828b6f
felipedoss <felipecaetano25.1996@gmail.com>
2020-07-31 16:10:29 -0300

    add Main

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
-import { Main, Details } from './pages';
+import MainRecipes from './pages/ MainRecipes/index';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Login} />
-        <Route path="/:type" component={Main} />
-        <Route path="/:type/:id" component={Details} />
+        <Route
+          exact
+          path="/comidas"
+          render={(props) => <MainRecipes props={props} type="meal" />}
+        />
+        <Route path="/:type/:id" component={Login} />
       </Switch>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { RecipesProvider } from './contexts/RecipesContext';
 
 // prettier-ignore
 ReactDOM.render(
-  <App />,
+  <RecipesProvider>
+    <App />
+  </RecipesProvider>,
   document.getElementById('root'),
 );
