bc64f4c545d9f094bae6e72413fa8d0611fbe5d3
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-07-31 16:15:48 -0300

    fixing app 'import'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
 import { Login, Main, Details } from './pages';
 
 function App() {
