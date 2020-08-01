77c46b9ffdac6652bcd18d2d58daac024819c391
felipedoss <felipecaetano25.1996@gmail.com>
2020-07-31 21:06:19 -0300

    update CardRecipes

--- src/components/CardRecipes.jsx
+++ src/components/CardRecipes.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
 
-const CardRecipes = ({ recipes }) => (
+const CardRecipes = ({ recipes, history }) => (
   <div>
     {recipes.map((recipe, index) => (
-      <li key={recipe.id} data-testid={`${index}-recipe-card`}>
-        <img src={recipe.image} alt="imagem" data-testid={`${index}-card-img`} />
-        <p data-testid={`${index}-card-name`}>{recipes.name}</p>
+      <li key={recipe.id}>
+        <button
+          type="button"
+          data-testid={`${index}-recipe-card`}
+          onClick={() => history.push(`/comidas/${recipe.id}`)}
+        >
+          <img src={recipe.image} alt="imagem" data-testid={`${index}-card-img`} />
+          <p data-testid={`${index}-card-name`}>{recipe.name}</p>
+        </button>
       </li>
     ))}
   </div>
@@ -14,5 +21,6 @@ const CardRecipes = ({ recipes }) => (
 
 CardRecipes.propTypes = {
   recipes: PropTypes.arrayOf(PropTypes.object).isRequired,
+  history: PropTypes.objectOf(PropTypes.any).isRequired,
 };
-export default CardRecipes;
+export default withRouter(CardRecipes);
