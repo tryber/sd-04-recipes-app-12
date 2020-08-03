679e7281f47b7795bcca21f8300997b5e091c58b
felipedoss <felipecaetano25.1996@gmail.com>
2020-08-02 22:54:58 -0300

    update CardRecipes

--- src/components/CardRecipes.jsx
+++ src/components/CardRecipes.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { withRouter } from 'react-router-dom';
+
 import { useRecipes } from '../contexts/RecipesContext';
 
-const CardRecipes = ({ history, type }) => {
+const CardRecipes = ({ type }) => {
   const { recipes, loading } = useRecipes();
   return !loading ? (
     <div>
       {recipes.map((recipe, index) => (
-        <button
-          key={recipe.id}
-          type="button"
-          data-testid={`${index}-recipe-card`}
-          onClick={
-            // prettier-ignore
-            () => history.push(
-              `${type === 'meal' ? '/comidas/' : '/bebidas/'}${recipe.id}`,
-            )
-          }
-        >
-          <img src={recipe.image} alt="imagem" data-testid={`${index}-card-img`} />
-          <p data-testid={`${index}-card-name`}>{recipe.name}</p>
-        </button>
+        <Link to={`${type === 'meal' ? '/comidas/' : '/bebidas/'}${recipe.id}`}>
+          <div key={recipe.id} type="button" data-testid={`${index}-recipe-card`}>
+            <img src={recipe.image} alt="imagem" data-testid={`${index}-card-img`} />
+            <p data-testid={`${index}-card-name`}>{recipe.name}</p>
+          </div>
+        </Link>
       ))}
     </div>
   ) : (
@@ -30,7 +23,6 @@ const CardRecipes = ({ history, type }) => {
 };
 
 CardRecipes.propTypes = {
-  history: PropTypes.objectOf(PropTypes.any).isRequired,
   type: PropTypes.string.isRequired,
 };
 
