048c5aff38246c6f1a9944da8b2d1402581cb39c
gabriellukke <gabriellukke@outlook.com>
2020-08-03 16:48:26 -0300

    Fix imports

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { drinkIcon, exploreIcon, mealIcon } from '../images/';
+import { drinkIcon, exploreIcon, mealIcon } from '../images';
+import './CSS/BottomMenu.css';
 
-const BottomMenu = () => {
-  return (
-    <footer>
-      <Link to="/bebidas">
-        <img src={drinkIcon} alt="drink-icon" />
-      </Link>
-      <Link to="/explorar">
-        <img src={exploreIcon} alt="explore-icon" />
-      </Link>
-      <Link to="/comidas">
-        <img src={mealIcon} alt="meal-icon" />
-      </Link>
-    </footer>
-  )
-}
+const BottomMenu = () => (
+  <footer>
+    <Link to="/bebidas">
+      <img src={drinkIcon} alt="drink-icon" />
+    </Link>
+    <Link to="/explorar">
+      <img src={exploreIcon} alt="explore-icon" />
+    </Link>
+    <Link to="/comidas">
+      <img src={mealIcon} alt="meal-icon" />
+    </Link>
+  </footer>
+);
 
 export default BottomMenu;
