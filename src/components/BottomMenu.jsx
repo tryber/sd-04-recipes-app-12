4311125666c14526040521ccf64d073fc2817f9a
gabriellukke <gabriellukke@outlook.com>
2020-08-03 17:04:20 -0300

    Initial CSS

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -4,8 +4,8 @@ import { drinkIcon, exploreIcon, mealIcon } from '../images';
 import './CSS/BottomMenu.css';
 
 const BottomMenu = () => (
-  <footer>
-    <Link to="/bebidas">
+  <footer className="footer">
+    <Link className="btn" to="/bebidas">
       <img src={drinkIcon} alt="drink-icon" />
     </Link>
     <Link to="/explorar">
