f167cddbd051844991a09ce2ea84f4651ccc3a0a
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-08-04 20:36:28 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
-import { Login, MainRecipes, Details } from './pages';
+import { Login, MainRecipes, Details, Profile } from './pages';
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Login} />
+        <Route path="/perfil" component={Profile} />
         <Route
           exact
           path="/comidas"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Login } from './Login/index';
 export { default as MainRecipes } from './MainRecipes/index';
 export { default as Details } from './DetailsRecipe';
+export { default as Profile } from './Profile';
