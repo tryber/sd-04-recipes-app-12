03f49f708ac329eaefe45a6bb6ecb407fcb47bb7
Eduardo dos Santos Flores <62265259+EduardoSantosF@users.noreply.github.com>
2020-08-10 17:19:40 -0300

    Done

--- src/pages/Explore/index.jsx
+++ src/pages/Explore/index.jsx
@@ -48,7 +48,7 @@ export default function Explore() {
   return (
     <div>
       <Header disableSearch={by !== 'area'} />
-      {renderButtons(type, by)}
+      {renderButtons()}
       <BottomMenu />
     </div>
   );
